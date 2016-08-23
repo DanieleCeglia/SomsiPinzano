abd0e9f57149c2d4b4bd57d06501f214595c5547
DanieleCeglia <daniele@iMac-di-Daniele-Ceglia.local>
2016-08-23 22:33:09 +0200

    inizio migrazione a singolo fragment dinamico

--- SomsiPinzano/app/src/main/java/dcsoft/somsipinzano/MainActivity.java
+++ SomsiPinzano/app/src/main/java/dcsoft/somsipinzano/MainActivity.java
@@ -1,12 +1,13 @@
 package dcsoft.somsipinzano;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnMenuTabClickListener;
@@ -15,13 +16,10 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity ";
     private CoordinatorLayout coordinatorLayout;
     private BottomBar mBottomBar;
-    private View cView;
-    private View gmView;
-    private View opmView;
 
-    public CategoriaFragment categoriaFragment;
-    public GoogleMapsFragment googleMapsFragment;
-    public OpenStreetMapFragment openStreetMapFragment;
+    public CategoriaFragment categoriaFragment = new CategoriaFragment();
+    public GoogleMapsFragment googleMapsFragment = new GoogleMapsFragment();
+    public OpenStreetMapFragment openStreetMapFragment = new OpenStreetMapFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,17 +30,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         coordinatorLayout = (CoordinatorLayout) findViewById(R.id.activity_tre_tab);
-
-        cView = categoriaFragment.getView();
-        gmView = googleMapsFragment.getView();
-        opmView = openStreetMapFragment.getView();
-
-        assert cView != null;
-        assert gmView != null;
-        assert opmView != null;
-
-        gmView.setVisibility(View.GONE);
-        opmView.setVisibility(View.GONE);
 
         mBottomBar = BottomBar.attach(this, savedInstanceState);
         mBottomBar.setItems(R.menu.menu_tre_tab);
@@ -55,9 +42,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onMenuTabReSelected(@IdRes int menuItemId) {
                 eseguiAzione(menuItemId);
-
             }
         });
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_categoria, categoriaFragment);
+        fragmentTransaction.commit();
     }
 
     @Override
@@ -73,26 +64,14 @@ public class MainActivity extends AppCompatActivity {
         switch (menuItemId) {
             case R.id.item_pdi: {
                 Snackbar.make(coordinatorLayout, R.string.punti_di_interesse, Snackbar.LENGTH_LONG).show();
-
-                cView.setVisibility(View.VISIBLE);
-                gmView.setVisibility(View.GONE);
-                opmView.setVisibility(View.GONE);
             }
             break;
             case R.id.item_google_maps: {
                 Snackbar.make(coordinatorLayout, "Google Maps", Snackbar.LENGTH_LONG).show();
-
-                cView.setVisibility(View.GONE);
-                gmView.setVisibility(View.VISIBLE);
-                opmView.setVisibility(View.GONE);
             }
             break;
             case R.id.item_open_street_map: {
                 Snackbar.make(coordinatorLayout, "OpenStreetMap", Snackbar.LENGTH_LONG).show();
-
-                cView.setVisibility(View.GONE);
-                gmView.setVisibility(View.GONE);
-                opmView.setVisibility(View.VISIBLE);
             }
         }
     }
