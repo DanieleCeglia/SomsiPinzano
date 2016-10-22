5ac481cd8d37bed88f723f8fce598180c7851587
DanieleCeglia <daniele.rulez@gmail.com>
2016-10-22 15:33:18 +0200

    Prima bozza richiesta permessi + gradle osmdroid

--- SomsiPinzano/app/src/main/java/dcsoft/somsipinzano/MainActivity.java
+++ SomsiPinzano/app/src/main/java/dcsoft/somsipinzano/MainActivity.java
@@ -1,23 +1,36 @@
 package dcsoft.somsipinzano;
 
+import android.Manifest;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnTabReselectListener;
 import com.roughike.bottombar.OnTabSelectListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     public BottomBar bottomBar;
     public Categoria categoriaScelta;
 
     private static final String TAG = "MainActivity ";
+    final private int REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS = 124;
     private FragmentManager fragmentManager;
     private ActionBar actionBar;
     private CategoriaFragment categoriaFragment;
@@ -31,6 +44,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         //Log.d("DEBUGAPP", TAG + "onCreate");
+
+        controllaPermessi();
 
         setContentView(R.layout.activity_main);
 
@@ -90,6 +105,40 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS: {
+                Map<String, Integer> perms = new HashMap<>();
+
+                perms.put(Manifest.permission.ACCESS_FINE_LOCATION, PackageManager.PERMISSION_GRANTED);
+                perms.put(Manifest.permission.WRITE_EXTERNAL_STORAGE, PackageManager.PERMISSION_GRANTED);
+
+                for (int i = 0; i < permissions.length; i++) {
+                    perms.put(permissions[i], grantResults[i]);
+                }
+
+                Boolean location = perms.get(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+                Boolean storage = perms.get(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+
+                if (location && storage) {
+                    Toast.makeText(MainActivity.this,  getResources().getString(R.string.messaggio_permessi_accettati), Toast.LENGTH_SHORT).show();
+                } else if (location) {
+                    Toast.makeText(this, getResources().getString(R.string.messaggio_permesso_di_archiviazione_non_accettato), Toast.LENGTH_LONG).show();
+                } else if (storage) {
+                    Toast.makeText(this, getResources().getString(R.string.messaggio_permesso_di_localizzazione_non_accettato), Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this, getResources().getString(R.string.messaggio_permesso_di_archiviazione_non_accettato) + "\n\n"
+                            + getResources().getString(R.string.messaggio_permesso_di_localizzazione_non_accettato), Toast.LENGTH_LONG).show();
+                }
+            }
+            break;
+
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
 
@@ -146,6 +195,24 @@ public class MainActivity extends AppCompatActivity {
     //endregion
 
     //region Metodi privati
+    private void controllaPermessi() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {// se è Android 6.0 o superiore...
+            List<String> permessiDaRichiedere = new ArrayList<>();
+
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                permessiDaRichiedere.add(Manifest.permission.ACCESS_FINE_LOCATION);
+            }
+
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                permessiDaRichiedere.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            }
+
+            if (!permessiDaRichiedere.isEmpty()) {
+                requestPermissions(permessiDaRichiedere.toArray(new String[permessiDaRichiedere.size()]), REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS);
+            }
+        } // else: ci sono già i permessi, quindi non c'è niente da fare!
+    }
+
     private void nascondiCategoriaFragment(FragmentTransaction fragmentTransaction) {
         if (categoriaFragment!= null && categoriaFragment.isAdded()) {
             fragmentTransaction.hide(categoriaFragment);
