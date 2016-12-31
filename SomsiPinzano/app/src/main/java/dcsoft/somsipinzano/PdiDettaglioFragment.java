389c9cbe379b238610c2b975c2c6aa4a2ec944ba
DanieleCeglia <daniele.rulez@gmail.com>
2016-12-31 19:47:21 +0100

    Avanzamenti vari su dettaglio PDI
    + DA VERIFICARE:
    • memoria scroll dettaglio pdi dopo rotazione è + quello che non va che
    quello che va
    • punto utente (omino) su OSM che salta fuori, ma dopo parecchio tempo
    (a volte non salta fuori proprio)
    • problemi occasionali rotazione su entrambe le mappe (dopo varie
    rotazioni, GM fa vedere mondo, OSM si posiziona circa sopra l'antartide)

--- SomsiPinzano/app/src/main/java/dcsoft/somsipinzano/PdiDettaglioFragment.java
+++ SomsiPinzano/app/src/main/java/dcsoft/somsipinzano/PdiDettaglioFragment.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
@@ -19,7 +20,32 @@ public class PdiDettaglioFragment extends Fragment {
     private MainActivity mainActivity;
 
     private ScrollView svContenitore;
+
+    private TextView tvIntestazioneDescrizione;
     private TextView tvDescrizione;
+
+    private LinearLayout llIndirizzo;
+    private TextView tvIndirizzo;
+
+    private LinearLayout llTelefono;
+    private TextView tvTelefono;
+
+    private LinearLayout llFax;
+    private TextView tvFax;
+
+    private LinearLayout llCellulare;
+    private TextView tvCellulare;
+
+    private LinearLayout llEmail;
+    private TextView tvEmail;
+
+    private LinearLayout llLink;
+    private TextView tvIntestazioneLink;
+    private TextView tvLink;
+
+    private LinearLayout llVideo;
+    private TextView tvVideo;
+
     private Button bVediSuGM;
     private Button bVediSuOSM;
 
@@ -55,10 +81,35 @@ public class PdiDettaglioFragment extends Fragment {
 
         pdiDettaglioFragmentView = inflater.inflate(R.layout.fragment_pdi_dettaglio, container, false);
 
-        svContenitore = (ScrollView) pdiDettaglioFragmentView.findViewById(R.id.svContenitore);
-        tvDescrizione = (TextView)   pdiDettaglioFragmentView.findViewById(R.id.tvDescrizione);
-        bVediSuGM     = (Button)     pdiDettaglioFragmentView.findViewById(R.id.bVediSuGM);
-        bVediSuOSM    = (Button)     pdiDettaglioFragmentView.findViewById(R.id.bVediSuOSM);
+        svContenitore             = (ScrollView)   pdiDettaglioFragmentView.findViewById(R.id.svContenitore);
+
+        tvIntestazioneDescrizione = (TextView)     pdiDettaglioFragmentView.findViewById(R.id.tvIntestazioneDescrizione);
+        tvDescrizione             = (TextView)     pdiDettaglioFragmentView.findViewById(R.id.tvDescrizione);
+
+        llIndirizzo               = (LinearLayout) pdiDettaglioFragmentView.findViewById(R.id.llIndirizzo);
+        tvIndirizzo               = (TextView)     pdiDettaglioFragmentView.findViewById(R.id.tvIndirizzo);
+
+        llTelefono                = (LinearLayout) pdiDettaglioFragmentView.findViewById(R.id.llTelefono);
+        tvTelefono                = (TextView)     pdiDettaglioFragmentView.findViewById(R.id.tvTelefono);
+
+        llFax                     = (LinearLayout) pdiDettaglioFragmentView.findViewById(R.id.llFax);
+        tvFax                     = (TextView)     pdiDettaglioFragmentView.findViewById(R.id.tvFax);
+
+        llCellulare               = (LinearLayout) pdiDettaglioFragmentView.findViewById(R.id.llCellulare);
+        tvCellulare               = (TextView)     pdiDettaglioFragmentView.findViewById(R.id.tvCellulare);
+
+        llEmail                   = (LinearLayout) pdiDettaglioFragmentView.findViewById(R.id.llEmail);
+        tvEmail                   = (TextView)     pdiDettaglioFragmentView.findViewById(R.id.tvEmail);
+
+        llLink                    = (LinearLayout) pdiDettaglioFragmentView.findViewById(R.id.llLink);
+        tvIntestazioneLink        = (TextView)     pdiDettaglioFragmentView.findViewById(R.id.tvIntestazioneLink);
+        tvLink                    = (TextView)     pdiDettaglioFragmentView.findViewById(R.id.tvLink);
+
+        llVideo                   = (LinearLayout) pdiDettaglioFragmentView.findViewById(R.id.llVideo);
+        tvVideo                   = (TextView)     pdiDettaglioFragmentView.findViewById(R.id.tvVideo);
+
+        bVediSuGM                 = (Button)       pdiDettaglioFragmentView.findViewById(R.id.bVediSuGM);
+        bVediSuOSM                = (Button)       pdiDettaglioFragmentView.findViewById(R.id.bVediSuOSM);
 
         if (bVediSuGM != null) {
             bVediSuGM.setOnClickListener(
@@ -82,8 +133,7 @@ public class PdiDettaglioFragment extends Fragment {
             );
         }
 
-        if (savedInstanceState == null) {
-        } else {
+        if (savedInstanceState != null) {
             //Log.d("DEBUGAPP", TAG + " onCreateView savedInstanceState != null");
 
             svContenitore.setY(savedInstanceState.getFloat("svContenitoreY"));
@@ -120,12 +170,30 @@ public class PdiDettaglioFragment extends Fragment {
             }
         }
 
+        String indirizzo = mainActivity.pdiScelto.getCitta() + ", " + mainActivity.pdiScelto.getVia();
+
+        if (mainActivity.pdiScelto.getNumeroCivico() > 0) {
+            indirizzo = indirizzo + ", " + mainActivity.pdiScelto.getNumeroCivico();
+        }
+
+        if (mainActivity.pdiScelto.getInterno() != null) {
+            indirizzo = indirizzo + "/" + mainActivity.pdiScelto.getInterno();
+        }
+
+        indirizzo = indirizzo + ", " + mainActivity.pdiScelto.getCap();
+        tvIndirizzo.setText(indirizzo);
+
         switch (mainActivity.gestoreDatabaseCondiviso.getLingua()) {
             case "italiano": {
                 if (mainActivity.pdiScelto.getDescrizioneItaliano() != null) {
                     tvDescrizione.setText(mainActivity.pdiScelto.getDescrizioneItaliano());
                 } else {
-                    tvDescrizione.setText("");
+                    tvIntestazioneDescrizione.setVisibility(View.GONE);
+                    tvDescrizione.setVisibility(View.GONE);
+                }
+
+                if (mainActivity.pdiScelto.getTitoloLinkGenericoItaliano() != null) {
+                    tvIntestazioneLink.setText(mainActivity.pdiScelto.getTitoloLinkGenericoItaliano() + ":");
                 }
             }
             break;
@@ -134,9 +202,50 @@ public class PdiDettaglioFragment extends Fragment {
                 if (mainActivity.pdiScelto.getDescrizioneInglese() != null) {
                     tvDescrizione.setText(mainActivity.pdiScelto.getDescrizioneInglese());
                 } else {
-                    tvDescrizione.setText("");
+                    tvIntestazioneDescrizione.setVisibility(View.GONE);
+                    tvDescrizione.setVisibility(View.GONE);
+                }
+
+                if (mainActivity.pdiScelto.getTitoloLinkGenericoInglese() != null) {
+                    tvIntestazioneLink.setText(mainActivity.pdiScelto.getTitoloLinkGenericoInglese() + ":");
                 }
             }
+        }
+
+        if (mainActivity.pdiScelto.getTelefono() != null) {
+            tvTelefono.setText(mainActivity.pdiScelto.getTelefono());
+        } else {
+            llTelefono.setVisibility(View.GONE);
+        }
+
+        if (mainActivity.pdiScelto.getFax() != null) {
+            tvFax.setText(mainActivity.pdiScelto.getFax());
+        } else {
+            llFax.setVisibility(View.GONE);
+        }
+
+        if (mainActivity.pdiScelto.getCellulare() != null) {
+            tvCellulare.setText(mainActivity.pdiScelto.getCellulare());
+        } else {
+            llCellulare.setVisibility(View.GONE);
+        }
+
+        if (mainActivity.pdiScelto.getEmail() != null) {
+            tvEmail.setText(mainActivity.pdiScelto.getEmail());
+        } else {
+            llEmail.setVisibility(View.GONE);
+        }
+
+        if (mainActivity.pdiScelto.getLinkGenerico() != null) {
+            tvLink.setText(mainActivity.pdiScelto.getLinkGenerico());
+        } else {
+            llLink.setVisibility(View.GONE);
+        }
+
+        if (mainActivity.pdiScelto.getLinkVideo() != null) {
+            tvVideo.setText(mainActivity.pdiScelto.getLinkVideo());
+        } else {
+            llVideo.setVisibility(View.GONE);
         }
     }
 
